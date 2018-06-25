b063b3129798b80e9f16df4077809627f5782f5c
Maria Pena-Guerrero <pena@stsci.edu>
2018-06-25 14:21:49 -0400

    Added a check to convert total running time to mins OR hrs.

--- calwebb_spec2_pytests/core_utils.py
+++ calwebb_spec2_pytests/core_utils.py
@@ -270,7 +270,7 @@ def start_end_PTT_time(txt_name, start_time=None, end_time=None):
             PTT_total_run_time = repr(PTT_total_time_not_in_sec)+"min"
         if PTT_total_time_not_in_sec > 60.0:
             PTT_total_time_not_in_sec = round(PTT_total_time / 60.0, 2)   # in hrs
-            PTT_total_run_time = repr(PTT_total_time_not_in_sec)+"hrs"
+            PTT_total_run_time = repr(PTT_total_time_not_in_sec)+"hr"
         print ("The total time for PTT to run (including pipeline) was "+repr(PTT_total_time)+" seconds.")
         if "full_run_map" not in txt_name:
             line2write = "{:<20} {:<20} {:<20} {:<20}".format('', '', 'PTT_total_run_time  ', repr(PTT_total_time)+'  ='+PTT_total_run_time)
