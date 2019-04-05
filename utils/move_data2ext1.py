e46c81a24c84ed4cbd2580ace27688eaa17d1abb
Maria Pena-Guerrero <pena@stsci.edu>
2019-04-04 22:12:25 -0400

    added reference pixel correction

--- utils/move_data2ext1.py
+++ utils/move_data2ext1.py
@@ -1,4 +1,5 @@
 import argparse
+import numpy as np
 from astropy.io import fits
 
 
@@ -9,7 +10,7 @@ This script moves the science data to extension 1.
 Example usage:
     The code works from the terminal.
     To create a NEW fits file with the updated header type:
-        > python /path_to_this_script/hdr_keywd_check.py blah.fits NRS1
+        > python /path_to_this_script/move_data2ext1.py blah.fits NRS1
         NRS1 (491) is the detector for which the data is being extracted, NRS2 (492) can also be used.
 
 
@@ -45,22 +46,43 @@ def move_data(input_fits_file, detector='NRS1'):
     if '2' in detector:
         det = 4
     input_fits_file_data = fits.getdata(input_fits_file, det)
-    outfile_ext = fits.ImageHDU(input_fits_file_data, name="SCI")
+    science_data = add_ref_pixels(input_fits_file_data)
+    outfile_ext = fits.ImageHDU(science_data, name="SCI")
     outfile.append(outfile_ext)
     # do the same for the error extension
     input_fits_file_data = fits.getdata(input_fits_file, det+1)
-    outfile_ext = fits.ImageHDU(input_fits_file_data, name="ERR")
+    error_data = add_ref_pixels(input_fits_file_data)
+    outfile_ext = fits.ImageHDU(error_data, name="ERR")
     outfile.append(outfile_ext)
     # do the same for the data quality extension
     input_fits_file_data = fits.getdata(input_fits_file, det+2)
-    outfile_ext = fits.ImageHDU(input_fits_file_data, name="DQ")
+    dq_data = add_ref_pixels(input_fits_file_data)
+    outfile_ext = fits.ImageHDU(dq_data, name="DQ")
     outfile.append(outfile_ext)
 
     # write the new output file
-    outfile_name = input_fits_file.replace(".fits", "_NRS"+repr(det)+"_modified.fits")
+    outfile_name = input_fits_file.replace(".fits", "_"+detector+"_modified.fits")
     outfile.writeto(outfile_name, overwrite=True)
 
 
+def add_ref_pixels(data):
+    """
+    This function adds the reference pixels to the given data array.
+    Args:
+        data: the science, DQ, or error data
+
+    Returns:
+
+    """
+    # define the correct size
+    data_out = np.zeros((2048,2048))
+
+    # add the reference pixels
+    for i in range(2040):
+        for j in range(2040):
+            data_out[j+4,i+1] = data[j,i]
+
+    return data_out
 
 
 
